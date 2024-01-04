cc604dc40e2a84d2b4ad45bd1cc7a7fac8442356
tanvirislamrakib <155340897+tanvirislamrakib@users.noreply.github.com>
2024-01-04 15:25:03 +0600

    Add files via upload

---
+++ indexMethod.js
@@ -0,0 +1,3 @@
+//Letter index number
+var fd = "rakib jubair osman rabbi sikhor"
+console.log(fd.indexOf("sikhor"));
---
+++ spiltMethod.js
@@ -0,0 +1,10 @@
+//Without use spilt method
+var friends = "Rakib, Akib, Tanvir, Sadhin, Siam";
+console.log(friends)
+
+//Use spilt method for removing any data
+var frnd = "rakib, jubair, osman, rabbi, sikhor"
+console.log(frnd.split("osman"));
+
+var fd = "rakib jubair osman rabbi sikhor"
+console.log(fd.split("rakib"));
